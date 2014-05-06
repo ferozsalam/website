10117bbb58f293717f333a25a62f7fcf00b7e9b7
Feroz Salam <feroz@zaytoun.co>
2014-05-06 22:10:31 +0100

    Add in new index and header PHP

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="nav">
+  <div class="heading">
+    <a href="/">FEROZ SALAM</a>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
+  </div>
+  <div>
+    <table class="menu">
+    <tr>
+    <td><a href="/projects">Projects</a></td>
+    </tr>
+    </table>
+  </div>
+</div>
+<br>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+  <title>Feroz Salam</title>
+</head>
+<body>
+  <script type="text/javascript">//loadContent()</script> 
+  <?php require('header.php');?>
+  <div class="content">
+    <p>
+    I work on internet security (specifically anti-phishing services) 
+    at <a href="http://www.netcraft.com">Netcraft</a> and study 
+    <a href="http://www.ed.ac.uk/schools-departments/history-classics-archaeology/graduate-school/programmes/history/taught-msc-history-online">History at 
+    the University of Edinburgh</a>. If I'm not working or studying, I'm probably 
+    messing with one of my 
+    <a href="http://www.ferozsalam.com/projects/">projects</a>, 
+    or trying to hit my Goodreads target for the year. 
+    </p>
+    <p>
+    If you would like to get in touch about my projects, writing, or anything else, 
+    my email address is <a href="mailto:feroz@zaytoun.co">feroz@zaytoun.co</a>. I can also be 
+    found at Twitter, as <a href="https://www.twitter.com/ferozsalam">@ferozsalam</a>.
+    </p>
+  </div>
+</body>
+</html>
