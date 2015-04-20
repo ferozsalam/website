f1c0dcf02c3e8591208d0b476f946e8384d11f5f
Feroz Salam <feroz@zaytoun.co>
2015-04-20 06:49:17 +0100

    Font changes, remove the unnecessary <hr>

--- header.php
+++ header.php
@@ -2,8 +2,7 @@
   <div class="heading">
     <a href="/">FEROZ SALAM</a>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
   </div>
-  <hr>
 </div>
 <br>
