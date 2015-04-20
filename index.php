fd7265d930119cc05561f2642398381a567a942d
Feroz Salam <feroz@zaytoun.co>
2015-04-20 20:13:14 +0100

    Use a wrapper div for margins on small viewports

--- index.php
+++ index.php
@@ -7,23 +7,25 @@
   <title>Feroz Salam</title>
 </head>
 <body>
-  <?php require('header.php');?>
-  <div class="content">
-    <p>
-    I work on internet security (specifically anti-phishing services) 
-    at <a href="http://www.netcraft.com">Netcraft</a><!-- and study 
-    <a href="http://www.ed.ac.uk/schools-departments/history-classics-archaeology/graduate-school/programmes/history/taught-msc-history-online">History at 
-    the University of Edinburgh</a>--!>. If I'm not working or studying, I'm probably 
-    messing with one of my 
-    <a href="http://www.ferozsalam.com/projects/">projects</a>, 
-    or trying to hit my Goodreads target for the year. 
-    </p>
-    <p>
-    If you would like to get in touch about my projects, writing, or anything else, 
-    my email address is <a href="mailto:feroz@argh.in">feroz@argh.in</a>. I can also be 
-    found at Twitter, as <a href="https://www.twitter.com/ferozsalam">@ferozsalam</a>. In the
-    event that this needs to be encrypted, my public key is <a href="pk.asc">here</a>.
-    </p>
+  <div class="body-wrapper">
+    <?php require('header.php');?>
+    <div class="content">
+      <p>
+      I work on internet security (specifically anti-phishing services) 
+      at <a href="http://www.netcraft.com">Netcraft</a><!-- and study 
+      <a href="http://www.ed.ac.uk/schools-departments/history-classics-archaeology/graduate-school/programmes/history/taught-msc-history-online">History at 
+      the University of Edinburgh</a>--!>. If I'm not working or studying, I'm probably 
+      messing with one of my 
+      <a href="http://www.ferozsalam.com/projects/">projects</a>, 
+      or trying to hit my Goodreads target for the year. 
+      </p>
+      <p>
+      If you would like to get in touch about my projects, writing, or anything else, 
+      my email address is <a href="mailto:feroz@argh.in">feroz@argh.in</a>. I can also be 
+      found at Twitter, as <a href="https://www.twitter.com/ferozsalam">@ferozsalam</a>. In the
+      event that this needs to be encrypted, my public key is <a href="pk.asc">here</a>.
+      </p>
+    </div>
   </div>
 </body>
 </html>
