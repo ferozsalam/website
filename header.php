327a731d17c8fa568f035938bf58c421ac3fb493
Feroz Salam <feroz@zaytoun.co>
2016-01-28 20:23:41 +0000

    Serve fonts over HTTPS

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <div class="nav">
   <div class="heading">
     <a href="/">FEROZ SALAM</a>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
   </div>
 </div>
 <br>
