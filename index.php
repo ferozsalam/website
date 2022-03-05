9006774341f3be0d5e179a6c915e143234ff012a
Feroz Salam <feroz@argh.in>
2022-03-05 21:35:27 +0000

    Update to current website state

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="style.css"/>
+  <link rel="stylesheet" type="text/css" href="https://seasonal-css.incoherency.co.uk/seasonal.css">
   <title>Feroz Salam</title>
 </head>
 <body>
   <div class="body-wrapper">
-    <?php require('header.php');?>
+  <?php require('nav.php');?>
     <div class="content">
-      <p>
-        I work on internet security at <a href="http://www.netcraft.com">Netcraft</a>. 
-        If I'm not working or studying, I'm probably messing with one of my 
-        <a href="http://www.ferozsalam.com/projects/">projects</a>.
-     </p>
-      <p>
-        If you would like to get in touch about my projects, writing, or anything else, 
-        my email address is <a href="mailto:feroz@argh.in">feroz@argh.in</a>. I can also be 
-        found at Twitter, as <a href="https://www.twitter.com/ferozsalam">@ferozsalam</a>. In the
-        event that whatever you want to say needs to be encrypted, my public key is <a href="pk.asc">here</a>.
-      </p>
+      <ul>
+        <li><a href="https://about.sourcegraph.com">Security Engineer at Sourcegraph</a></li>
+        <li><a href="https://padlock.argh.in">Security blog</a></li>
+        <li><a href="https://doh.li/">DNS over HTTPS server</a></li>
+        <li><a href="mailto:feroz@argh.in">Email</a></li>
+        <li><a href="https://twitter.com/ferozsalam">Twitter</a></li>
+      </ul>
     </div>
   </div>
 </body>
